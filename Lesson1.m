3c52ea1cc0315b7ca4fce36a372e67d1d18ed156
Anton Ovcharenko <antonovch@icloud.com>
2021-09-12 17:34:27 +0300

    dot-m file version of lesson 1

---
+++ Lesson1.m
@@ -0,0 +1,28 @@
+%% Lesson 1. Visualize the dataset
+% Your simulator have generated phase and amp matrices, using the patt (for pattern) images.
+% You load the data into the workspace:
+data = load('lesson1.mat');
+whos
+
+%% Task 1. Visualize random images on a plot in an array of NxN subplots.
+% Hint: use subplot in a loop, pcolor or imshow for displaying
+
+
+%% Task 2. Obtaian \Delta\phi and \phi_0 as difference on the edges and 
+% central phase and use average efficiency as the color.
+
+
+%% Task 3. Make a scatter plot of \phi_0 vs \Delta\phi. 
+
+
+%% Task 4. Use MATLAB's least square approximation algorithm for getting \Delta\phi and \phi_0.
+
+
+%% Task 5. Make the same scatter plot using function from Task 3. Compare results.
+
+
+%% Task 6. (At home) Repeat the steps for the second order approximation.
+% Hint 1: \Delta^2\phi is sum at the edges minus two central
+% Hint 2: Use either different scatter plots, or a 3D one (scatter3) to
+% visualize the 0, 1st and 2nd order coefficients.
+
