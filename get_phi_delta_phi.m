4a4ce4d74bb138f24f3b6a0a4fe380fac9722e3c
Anton Ovcharenko <antonovch@icloud.com>
2021-10-10 22:45:05 +0300

    added unwrap

--- get_phi_delta_phi.m
+++ get_phi_delta_phi.m
@@ -1,6 +1,7 @@
 function [phi0,delta_phi,ave] = get_phi_delta_phi(data, wavelength)
 
     phase = mod(angle(data),2*pi); % same as atan(imag(data)./real(data))
+    phase = unwrap(phase,[],1);
     amp = abs(data); % same as sqrt(real(data).^2 + imag(data).^2)
     [~, mid] = min(abs(wavelength - mean(wavelength)));
     phi0 = phase(mid,:);
