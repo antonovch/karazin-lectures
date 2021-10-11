bcef5b8d3584c705e1e5739ac6073b78c1d85f18
Anton Ovcharenko <antonovch@icloud.com>
2021-10-11 13:47:08 +0300

    modified pcolor options

--- Lesson0_solution.m
+++ Lesson0_solution.m
@@ -38,5 +38,5 @@ map(I) = lvls(ilvl);
 figure; pcolor(X,Y,map); shading flat; colorbar; axis equal
 
 %% Task 3. Test
-figure; pcolor(X,Y,map); shading flat; colorbar; axis equal
+figure; pcolor(X,Y,map2); colorbar; axis equal
 
