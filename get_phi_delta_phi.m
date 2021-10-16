9ee82752e001f264c98237701ecbbca975781b15
Anton Ovcharenko <antonovch@icloud.com>
2021-10-17 02:17:51 +0300

    minor refactoring

--- get_phi_delta_phi.m
+++ get_phi_delta_phi.m
@@ -1,7 +1,6 @@
 function [phi0,delta_phi,ave] = get_phi_delta_phi(data, wavelength)
 
-    phase = mod(angle(data),2*pi); % same as atan(imag(data)./real(data))
-    phase = unwrap(phase,[],1);
+    phase = unwrap(angle(data),[],1); % angle() is same as atan(imag(data)./real(data))
     amp = abs(data); % same as sqrt(real(data).^2 + imag(data).^2)
     [~, mid] = min(abs(wavelength - mean(wavelength)));
     phi0 = mod(phase(mid,:),2*pi);
